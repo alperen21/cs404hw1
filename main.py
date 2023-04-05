a69f9e4f608ec9ff3bb5332bd1cecfbc6bba5c2e
alperenyildiz <alperenyildiz@sabanciuniv.edu>
2023-04-05 16:40:43 +0300

    added logs

--- main.py
+++ main.py
@@ -9,24 +9,28 @@ from algorithm.Node import Node
 from memory_profiler import profile
 
 
-def start_ucs(difficulty : str) -> None:
+def start_ucs(difficulty : str, start : int, end : int) -> None:
     """
     Runs UCS over the selected difficulty
 
     :param difficulty: difficulty level
+    :param start: start of the range of matrices
+    :param difficulty: end of the range of matrices
     :returns: None
     """
-    for i in range(1,6):
+    for i in range(start,end):
         ucs(f"inputs/{difficulty}/{difficulty}{i}.txt")
 
-def start_a_star(difficulty : str) -> None:
+def start_a_star(difficulty : str, start : int, end : int) -> None:
     """
     Runs A* search over the selected difficulty
 
     :param difficulty: difficulty level
+    :param start: start of the range of matrices
+    :param difficulty: end of the range of matrices
     :returns: None
     """
-    for i in range(1,6):
+    for i in range(start,end):
         a_star(f"inputs/{difficulty}/{difficulty}{i}.txt")
 
 @profile(precision=4)
@@ -41,35 +45,38 @@ def main() -> None:
     parser = argparse.ArgumentParser(description='Use a search function to solve maze coloring problem')
     parser.add_argument('--difficulty', type=str, required=True)
     parser.add_argument('--algorithm', type=str, required=True)
+    parser.add_argument('--start', type=int, required=True)
+    parser.add_argument('--end', type=int, required=True)
+
     args = parser.parse_args()
     
     if args.algorithm == "ucs":
         if args.difficulty == "easy":
             print("using ucs with easy")
-            start_ucs("easy")
+            start_ucs("easy", args.start, args.end)
 
         elif args.difficulty == "medium":
             print("using ucs with medium")
-            start_ucs("medium")
+            start_ucs("medium", args.start, args.end)
 
         elif args.difficulty == "hard":
             print("using ucs with hard")
-            start_ucs("hard")
+            start_ucs("hard", args.start, args.end)
         else:
             parser.print_help()
             
     elif args.algorithm == "a_star":
         if args.difficulty == "easy":
             print("using A* with easy")
-            start_a_star("easy")
+            start_a_star("easy", args.start, args.end)
 
         elif args.difficulty == "medium":
             print("using A* with medium")
-            start_a_star("medium")
+            start_a_star("medium", args.start, args.end)
 
         elif args.difficulty == "hard":
             print("using A* with hard")
-            start_a_star("hard")
+            start_a_star("hard", args.start, args.end)
         else:
             parser.print_help()
     else:
---
+++ start.sh
@@ -0,0 +1,21 @@
+
+
+
+for i in $(seq 1 5); 
+    do 
+    j=$((i + 1));
+    python -m memory_profiler main.py --algorithm ucs --difficulty easy --start $i --end $j | tee ./logs/ucs_easy_$i.log;
+    python -m memory_profiler main.py --algorithm ucs --difficulty medium --start $i --end $j | tee ./logs/ucs_medium_$i.log;
+    python -m memory_profiler main.py --algorithm ucs --difficulty hard --start $i --end $j | tee ./logs/ucs_hard_$i.log;
+
+done
+
+for i in $(seq 1 5); 
+    do 
+    j=$((i + 1));
+    python -m memory_profiler main.py --algorithm a_star --difficulty easy --start $i --end $j | tee ./logs/a_star_easy_$i.log;
+    python -m memory_profiler main.py --algorithm ucs --difficulty medium --start $i --end $j | tee ./logs/a_star_medium_$i.log;
+    python -m memory_profiler main.py --algorithm ucs --difficulty hard --start $i --end $j | tee ./logs/a_star_hard_$i.log;
+
+
+done
